2d8f903bc89ee42c56d0e86018bd7409840d9840
miltonfelga <47542851+miltonfelga@users.noreply.github.com>
2020-05-15 20:32:59 -0300

    iniciando projeto pwfilmes

---
+++ src/components/header/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./Header.css";
+
+export default function Header (props) {
+    return(
+        <div class="Header">
+            <div classname="Logo">CoronaVideos</div>
+            <input type="text" name="search" id="search"></input>
+        </div>   
+)
+}
