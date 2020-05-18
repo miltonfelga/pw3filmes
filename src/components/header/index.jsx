851bc8088a320b47a4b2066be0cc5c8996bfa033
Igor Cruz <47676774+mjigormj@users.noreply.github.com>
2020-05-18 16:56:17 -0300

    navbar

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import "./Header.css";
+import Icon from "./pesquisa.png";
 
-export default function Header (props) {
-    return(
-        <div class="Header">
-            <div classname="Logo">CoronaVideos</div>
-            <input type="text" name="search" id="search"></input>
-        </div>   
-)
+export default function Header(props) {
+    return (
+        <div className="Header">
+            <div className="Logo">CoronaVideos</div>
+            <div className="searchBar">
+                <input type="text" name="search" id="search"></input>
+                <button className="Pesquisa"><img src={Icon} alt="Icon" /></button>
+            </div>
+        </div>
+    )
 }
