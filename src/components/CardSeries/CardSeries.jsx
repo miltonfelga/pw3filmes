c2966087bb9e16079f34c7bbf4fa5f4eef049a56
Igor Cruz <47676774+mjigormj@users.noreply.github.com>
2020-07-03 00:22:38 -0300

    css prt3 / coreção de bugs
    Tratamento de cards:
    cards sem imagem não são retornados;
    A falta de descrição é substituida por "Sem descrição"

--- src/components/CardSeries/CardSeries.jsx
+++ src/components/CardSeries/CardSeries.jsx
@@ -3,22 +3,27 @@ import './CardSeries.css'
 
 export default function CardSerie({ serie }) {
     const imgUrl = 'https://image.tmdb.org/t/p/w200/';
-    return (
-        <div className='cardSerie'>
-            <div>
-                <div className='posterImage' style={ {backgroundImage: `url(${imgUrl}${serie.poster_path})`}}>
-                    <div className="imageText">
-                        
-                        <p>{serie.overview.substring(0, 80)}...</p>
+    if (serie.poster_path != null) {
+        if (serie.overview === "") serie.overview = "Sem Descrição";
+        return (
+            <div className='cardSerie'>
+                <div>
+                    <div className='posterImage' style={{ backgroundImage: `url(${imgUrl}${serie.poster_path})` }}>
+                        <div className="imageText">
+                            {}
+                            <p>{serie.overview.substring(0, 80)}...</p>
+                        </div>
+                    </div>
+                    <div className='cardDetail'>
+                        <span className="material-icons">star</span>
+                        {serie.vote_average}
                     </div>
                 </div>
-                <div className='cardDetail'>
-                    <span className="material-icons">star</span>
-                    {serie.vote_average}
-                </div>
             </div>
-        </div>
-    )
+        )
+    } else {
+        return null;
+    }
 }
 
 
--- src/pages/lancamentos/Lancamentos.jsx
+++ src/pages/lancamentos/Lancamentos.jsx
@@ -12,6 +12,7 @@ export default function Lancamentos() {
         try {
             const resposta = await axios.get("https://api.themoviedb.org/3/tv/airing_today?api_key=c0d8598e0d3c812f79b7ca74fde1a95c&language=pt-BR");
             setSeries(resposta.data.results);
+            console.log(resposta.data.results);
         } catch (erro) {
             console.log(erro);
         }
